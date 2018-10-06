eff783020aae216386f8c27b27b4d138f8a1fa96
Yolanda Maya Gita <43692378+YolandaMayaGita@users.noreply.github.com>
2018-10-06 10:55:39 +0700

    Update hal_26_latihan_5.cpp

--- hal_26_latihan_5.cpp
+++ hal_26_latihan_5.cpp
@@ -11,13 +11,14 @@ main()
 	jum_bayar=0; cout<<"Total Pembelian Rp."; cin>>tot_beli;
 	cout<<endl<<endl;
 	
-	if (tot_beli >=  50000)
+	if (tot_beli >=  50000) {
 	    potongan = 0.2*tot_beli;
-	else
-	potongan = 0.05* tot_beli; 
-	
+	}
+	else 
+	potongan = 0.05* tot_beli; {
 	cout<<"Besarnya potongan Rp."<<potongan<<endl; 
 	jum_bayar = tot_beli - potongan;
+	}
 	cout<<endl<<endl;
 	
 	cout<<"Jumlah yang harus dibayarkan : Rp."<<jum_bayar<<endl;
